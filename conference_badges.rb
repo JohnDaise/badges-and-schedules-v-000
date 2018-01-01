a333daa36026a15052edb3cf10e9ac645a49a390
John Daise <jdaise@gmail.com>
2018-01-01 23:16:55 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.collect {|name| badge_maker(name)}
+end
+
+def assign_rooms(attendees)
+  attendees.each_with_index.map do |name, index|
+    "Hello, #{name}! You'll be assigned to room #{index+1}!"
+  end
+end
+
+def printer(attendees)
+  attendees.collect.inspect do |name| badge_maker(name)}
+
+  attendees.each_with_index.map.inspect do |name, index|
+    "Hello, #{name}! You'll be assigned to room #{index+1}!"
+  end
+
+end

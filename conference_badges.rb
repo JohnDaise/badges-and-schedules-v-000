a385b045990818f00b62cef276be14ac428c9779
John Daise <jdaise@gmail.com>
2018-01-01 23:17:25 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,7 +15,7 @@ end
 
 def printer(attendees)
   attendees.collect.inspect do |name| badge_maker(name)}
-
+  end
   attendees.each_with_index.map.inspect do |name, index|
     "Hello, #{name}! You'll be assigned to room #{index+1}!"
   end

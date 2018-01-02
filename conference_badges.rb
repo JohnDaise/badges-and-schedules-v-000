dddbf57097b6a399c52ee0f83fb17f6344582fa2
John Daise <jdaise@gmail.com>
2018-01-02 01:58:52 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,6 +14,6 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  puts batch_badge_creator(attendees)
+  attendees.collect.inspect {|name| badge_maker(name)}
 #  puts assign_rooms(attendees)
 end
